394382ec7716129e6e0aa3133742c6c263f25e05
Flashky <mevengar@gmail.com>
2018-12-28 22:50:29 +0100

    Implement getPacktOfferList in ApiCheckoutService (#52)

--- src/main/java/brv/notifier/packt/services/checking/ApiCheckoutService.java
+++ src/main/java/brv/notifier/packt/services/checking/ApiCheckoutService.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import brv.notifier.packt.mappers.Mapper;
 import brv.notifier.packt.model.PacktFreeOffer;
+import brv.notifier.packt.model.json.JsonOffer;
 import brv.notifier.packt.model.json.JsonOffers;
 import brv.notifier.packt.model.json.JsonSummary;
 import brv.notifier.packt.services.CheckoutService;
@@ -52,12 +53,28 @@ public class ApiCheckoutService implements CheckoutService {
 		return offer;
 	}
 	
+	
 	@Override
 	public List<PacktFreeOffer> getPacktOfferList(LocalDate start, int numberOfDays) {
 		
+		List<PacktFreeOffer> result = new ArrayList<>();
 		
-		// TODO hacer en un futuro
-		return new ArrayList<>();
+		// Obtain the list of upcoming offers
+		String endpointUrl = endpointManager.getOfferListEndpoint(LocalDate.now(), numberOfDays);
+		JsonOffers offerList = restTemplate.getForObject(endpointUrl, JsonOffers.class);
+		
+		if((offerList != null) && (!offerList.getData().isEmpty())) {
+			
+			for(JsonOffer jsonOffer : offerList.getData()) {
+				
+				endpointUrl = endpointManager.getSummaryEndpoint(jsonOffer.getProductId());
+				JsonSummary offerSummary = restTemplate.getForObject(endpointUrl, JsonSummary.class);
+				
+				result.add(Mapper.jsonToModel(offerSummary));
+			}
+		}
+		
+		return result;
 	}
 
 }
