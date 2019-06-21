724d8569ab118bfdc756a24b7d180d5ca9e9d3fd
Flashky <mevengar@gmail.com>
2019-06-21 18:43:20 +0200

    #80 Create EmailConfiguration class

--- src/main/java/brv/notifier/packt/NotifierPacktApplication.java
+++ src/main/java/brv/notifier/packt/NotifierPacktApplication.java
@@ -5,7 +5,6 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -16,9 +15,6 @@ import org.springframework.web.client.RestTemplate;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 import brv.notifier.packt.configuration.TwitterConfiguration;
-import brv.notifier.packt.notifications.DailyNotificationListener;
-import brv.notifier.packt.notifications.EmailNotificationListener;
-import brv.notifier.packt.services.PacktCheckTask;
 import brv.notifier.packt.util.MessageHelper;
 
 @SpringBootApplication
@@ -31,19 +27,6 @@ public class NotifierPacktApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotifierPacktApplication.class, args);
 	}
-	
-	
-	@Bean 	
-	@ConditionalOnProperty(value="notifications.email.enabled", havingValue = "true", matchIfMissing = true)
-	public EmailNotificationListener getEmailService(PacktCheckTask service) {
-		
-		DailyNotificationListener listener = new EmailNotificationListener();
-		service.addNotificationListener(listener);
-		
-		return (EmailNotificationListener) listener;
-	}
-	
-
 	
 	@Bean
 	public Locale getLocale() {
