e7a825b157a6d2e1c94a5fbde44bc1160889f220
Flashky <mevengar@gmail.com>
2019-06-21 20:59:10 +0200

    Fix code-style issues

--- src/main/java/brv/notifier/packt/constants/PlaceholderValue.java
+++ src/main/java/brv/notifier/packt/constants/PlaceholderValue.java
@@ -2,8 +2,9 @@ package brv.notifier.packt.constants;
 
 public final class PlaceholderValue {
 
-	private PlaceholderValue() {}
-	
 	public static final String SCHEDULE	= "${app.schedule}";
 	public static final String LANG 	= "${app.lang:en}";
+	
+	private PlaceholderValue() {}
+
 }
--- src/main/java/brv/notifier/packt/constants/PropertyName.java
+++ src/main/java/brv/notifier/packt/constants/PropertyName.java
@@ -2,13 +2,13 @@ package brv.notifier.packt.constants;
 
 public final class PropertyName {
 
-	private PropertyName() {}
-	
 	private static final String ENABLED = ".enabled";
 	
 	public static final String EMAIL = "app.notifications.email";
 	public static final String TWITTER = "app.notifications.twitter";
 	public static final String EMAIL_ENABLED = EMAIL + ENABLED;
 	public static final String TWITTER_ENABLED = TWITTER + ENABLED;
+	
+	private PropertyName() {}
 	
 }
