bbad32936463cab097de7a20894cfe7ad2422c9d
Flashky <mevengar@gmail.com>
2019-06-22 16:49:49 +0200

    #86 Twitter oneliner hashtagging to ignore case

--- src/main/java/brv/notifier/packt/notifications/TwitterNotificationListener.java
+++ src/main/java/brv/notifier/packt/notifications/TwitterNotificationListener.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +24,15 @@ import twitter4j.Twitter;
 public class TwitterNotificationListener implements DailyNotificationListener {
 
 	private static final Logger LOGGER = LogManager.getLogger(TwitterNotificationListener.class.getName());
-	private static final String HASHTAG = "#";
+	
 	private static final String EMOJI_BOOK = "open_book";
-		
+	
+	// Oneliner formatting
+	private static final String HASHTAG = "#";
+	private static final int MINIMUM_LENGTH = 2;
+	private static final int NO_MATCH = -1;
+	private static final String REGEX_WHITESPACES = "\\s+";
+	
 	@Autowired
 	private Twitter twitter;
 	
@@ -68,22 +75,31 @@ public class TwitterNotificationListener implements DailyNotificationListener {
 
 	private String formatOneliner(PacktFreeOffer offerData) {
 		
-		String title = offerData.getTitle();
-		String[] titleWords = title.split("\\s+");
+		String[] titleWords = offerData.getTitle().split(REGEX_WHITESPACES);
+		Set<String> matches = new HashSet<>();
 		String oneliner = offerData.getOneLiner();
 		
-		Set<String> matches = new HashSet<>();
+		StringBuilder onelinerBuilder = new StringBuilder(oneliner);
 		
 		for(String word : titleWords) {
-			if((!matches.contains(word)) && (word.length() > 2) && (oneliner.contains(word))) {
-				matches.add(word);
-				oneliner = oneliner.replaceFirst(word, HASHTAG+word);
+			if((!matches.contains(word)) && (word.length() > MINIMUM_LENGTH)) {
+				
+				int position = StringUtils.indexOfIgnoreCase(oneliner, word);
+				if(position != NO_MATCH) {
+					
+					// Append the hashtag before the character
+					onelinerBuilder.insert(position, HASHTAG);
+					oneliner = onelinerBuilder.toString();
+					
+					// Add the word to the set so I don't repeat hashtags
+					matches.add(word);
+				}
+				
 			}
 		}
 		
 		return oneliner;
-	}
-	
+	} 
 	
 	private void close(InputStream input) {
 		if(input != null) {
