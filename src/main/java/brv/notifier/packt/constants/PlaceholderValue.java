6af52c3c363e415a862ab0b7b46e779b4630537a
Flashky <mevengar@gmail.com>
2019-06-22 22:17:05 +0200

    Fix placeholder issue

--- src/main/java/brv/notifier/packt/constants/PlaceholderValue.java
+++ src/main/java/brv/notifier/packt/constants/PlaceholderValue.java
@@ -2,7 +2,7 @@ package brv.notifier.packt.constants;
 
 public final class PlaceholderValue {
 
-	public static final String CRON_EXPRESSION	= "${service.cron}";
+	public static final String CRON_EXPRESSION	= "${app.cron}";
 	public static final String LANG 	= "${app.lang:en}";
 	
 	private PlaceholderValue() {}
