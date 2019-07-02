81da8500025341dec209808e0e8869e71fd3cf91
Flashky <mevengar@gmail.com>
2019-07-02 22:05:10 +0200

    Add RestTemplate error handling #91

---
+++ src/main/java/brv/notifier/packt/repositories/PacktResponseErrorHandler.java
@@ -0,0 +1,40 @@
+package brv.notifier.packt.repositories;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
+
+@Component
+public class PacktResponseErrorHandler implements ResponseErrorHandler {
+
+	private static final Logger LOGGER = LogManager.getLogger(PacktResponseErrorHandler.class.getName());
+	
+	@Override
+	public boolean hasError(ClientHttpResponse response) throws IOException {
+		return response.getStatusCode().isError();
+	}
+
+	@Override
+	public void handleError(ClientHttpResponse response) throws IOException {
+		
+		switch(response.getStatusCode().series()) {
+		
+			case SERVER_ERROR:
+				LOGGER.error("Server side error when calling to the API ("+response.getStatusCode()+")");
+				break;
+				
+			case CLIENT_ERROR:
+				LOGGER.error("Client side error when calling to the API ("+response.getStatusCode()+")");
+				break;
+				
+			default:
+				break;
+		}
+		
+	}
+
+}
